94ccee413f2a43e0db5d8405c35fd09455d29231
Rahil Manzar <rahil.manzar@jabong.com>
2019-05-12 12:45:05 +0530

    email survey app patch

---
+++ config/dev.js
@@ -0,0 +1,11 @@
+module.exports = {
+    googleClientID: '1036408695774-o7rdmtaot359jh69o9obleti6vvjmktg.apps.googleusercontent.com',
+    googleClientIDProd: '338847850902-116h0t97t1hpnshgsheroeota91sf9o9.apps.googleusercontent.com',
+    googleClientSecret: 'bSfUsOx_2tee3JaP84iAGKmu',
+    googleClientSecretProd: 'g2MuxVdHq9Pz94OfOmvBD11p',
+    mongoURI: 'mongodb+srv://rahil:abcd123@cluster1-yknpd.mongodb.net/test?retryWrites=true',
+    mongoURIProd: 'mongodb+srv://abrinzer:12345@clusterprod-qv3ou.mongodb.net/test?retryWrites=true',
+    cookieKey: 'dhgdsgjhsdhjsgdjgskhsajshkah',
+    facebookClientId: 2097411737038024,
+    facebookClientSecret: '443cfcb587f1976eaef87d31ef41947b'
+};
